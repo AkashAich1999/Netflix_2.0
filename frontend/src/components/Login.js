4db8f0c49f9f4e431c8fe61b838efe3d48f9a5da
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-04-01 14:00:16 +0530

    Sign In & Sign Up Functionality Completed.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,67 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import axios from "axios";
+import { API_END_POINT } from "../utils/constant";
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
 
-  const getInputData = (e) => {
-     e.preventDefault();
-     console.log(fullName, email, password);
-     setFullName("");
-     setEmail("");
-     setPassword("");
-  }
+  const getInputData = async (e) => {
+    e.preventDefault();
+    if (isSignInForm) {
+      // Login
+      const user = { email, password };
+      try {
+        const res = await axios.post(`${API_END_POINT}/login`, user, {
+          headers:{
+            'Content-Type':'application/json'
+          },
+          withCredentials:true
+        });
+        console.log(res);
+        if(res.data.success){
+          toast.success(res.data.message);
+        }
+        navigate("/browse");
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      }     
+    } else {
+      // Register
+      const user = { fullName, email, password };
+      // console.log(user);
+      try {
+        const res = await axios.post(`${API_END_POINT}/register`, user, {
+          headers:{
+            'Content-Type':'application/json'
+          },
+          withCredentials:true
+        });
+        console.log(res);
+        if(res.data.success){
+          toast.success(res.data.message);
+        }
+        setIsSignInForm(true);
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      }
+    }
+    setFullName("");
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <div>
@@ -28,21 +72,44 @@ const Login = () => {
           alt="background_image"
         />
       </div>
-      <form onSubmit={getInputData} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-          <h1 className="font-bold text-3xl py-4">
-            {isSignInForm ? "Sign In" : "Sign Up"}
-          </h1>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
-          {!isSignInForm && (
-            <input value={fullName} onChange={(e) => setFullName(e.target.value)} type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700"/>
-          )}
-          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
-          <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-              {isSignInForm ? "Sign In" : "Sign Up"}
-          </button>
-          <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-              {isSignInForm ? "New to Netflix? Sign up now." : "Already Registered? Sign In Now."}
-          </p>
+      <form
+        onSubmit={getInputData}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        {!isSignInForm && (
+          <input
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign up now."
+            : "Already Registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
